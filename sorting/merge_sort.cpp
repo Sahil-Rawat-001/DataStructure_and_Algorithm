47d30bb6e7397e0cd784b52d43a88d87b660b326
Sahil-Rawat-001 <rawatsahilwork001@gmail.com>
2025-08-05 16:25:58 +0530

    add merge sort program

---
+++ sorting/merge_sort.cpp
@@ -0,0 +1,61 @@
+// Why merge sort over bubble sort, selection sort and insertion sort ?
+// because the time complexity is goes down to the O(nlogn);
+// It is much more optimized sorting algorithm and sort any kind of data.
+// Concept of merge sort - divide and merge
+
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+void mergeArr(vector<int>& arr,int l,int mid,int h){
+    vector<int> temp;
+    int left = l;
+    int right = mid+1;
+
+    while(left <= mid && right <= h){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while(right <= h){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i = l; i <= h; i++){
+        arr[i] = temp[i-l];
+    }
+
+}
+
+void mergeSort(vector<int>& arr,int l, int h){
+    if(l >= h) return;
+    int mid = (l+h)/2;
+    mergeSort(arr,l,mid);
+    mergeSort(arr,mid+1,h);
+    mergeArr(arr,l,mid,h);
+}
+
+int main(){
+    vector<int> arr = {3,1,2,4,1,5,2,6,4};
+    int l = 0;
+    int h = arr.size() -1;
+
+    mergeSort(arr,l,h);
+
+    for(auto val : arr){
+        cout << val << " ";
+    }
+    return 0;
+}
